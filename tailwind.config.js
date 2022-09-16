6800262d8c53a09c6033ab4f9d43be10c3214411
idaols <ida.olsson@metropolia.fi>
2022-09-16 21:50:52 +0300

    redi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./src/img/*.{jpg,svg}"],
+  content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     screens: {
       sm: "640px",
